45400e538014bd49dcde7f3dc283a9c947656b91
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-04-11 16:43:46 -0500

    Update vcf2fasta.py

--- vcf2fasta.py
+++ vcf2fasta.py
@@ -11,10 +11,10 @@ with open(outputFile,'w') as outFile:
     count =0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        outFile.write('module load vcftools/0.1 \n') 
         for row in csv_reader:
             if count !=0:
-                outFile.write(' module load vcftools/0.1 \n') 
-                outFile.write(f' cat $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa | vcf-consensus  $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz > $WORK/coocnet/fasta/{row[0]}.fa\n')
+                outFile.write(f'$WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa | vcf-consensus  $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz > $WORK/coocnet/fasta/{row[0]}.fa\n')
             count =count+1
             
             
