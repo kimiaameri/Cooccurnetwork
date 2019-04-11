c8d3585202efe30d93d7a9a7637103545f7e4aed
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-04-11 16:35:34 -0500

    Update vcf2fasta.py

---
+++ vcf2fasta.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write('No Input CSV file \n')
+    sys.exit(0)
+    
+inputFile = sys.argv[1]
+outputFile = "vcf2fasta.sh"
+with open(outputFile,'w') as outFile:
+    count =0
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if count !=0:
+                outFile.write(' module load vcftools/0.1 \n') 
+                outFile.write(f' cat $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa | vcf-consensus  $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz > $WORK/SAEVA-outputs/fasta/{row[0]}.fa\n')
+            count =count+1
+            
+            
+            
