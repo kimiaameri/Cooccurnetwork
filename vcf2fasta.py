468671b4926ebf73b1b1546d51aef4b9e77d21bc
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-04-18 13:59:00 -0500

    Update vcf2fasta.py

--- vcf2fasta.py
+++ vcf2fasta.py
@@ -11,11 +11,24 @@ with open(outputFile,'w') as outFile:
     count =0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
+        if count !=0:
+         outFile.write(f'bgzip {row[0]}.vcf\n')
+         outFile.write(f'tabix -p vcf {row[0]}.vcf.gz\n')
+        count =count+1
         outFile.write('module load samtools/1.6 \n') 
+        outFile.write('sed -i 's/^chr/Chromosome/' genome.fa\n') 
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'cat $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa | vcf-consensus  $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz > $WORK/coocnet/fasta/{row[0]}.fa\n')
+                outFile.write(f'cat $WORK/coocnet/genome.fa | vcf-consensus  $WORK/coocnet/vcf/{row[0]}.vcf.gz > $WORK/coocnet/fasta/{row[0]}.fa\n')
             count =count+1
             
             
+            
+            
+            
+             sed -i 's/^chr/Chromosome/' genome.fa
+                 bgzip S*.vcf
+
+
+            tabix -p vcf Silverman99624D1_S16_q5000_dp250.vcf.gz
             
